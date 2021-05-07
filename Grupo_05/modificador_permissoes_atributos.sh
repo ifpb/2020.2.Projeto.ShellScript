b641266ce767d7ac682b24fffd3ae9e1c765d83e
Mateus <mateusmota0100@gmail.com>
2021-05-07 14:32:09 -0300

    Adicionado menu principal.

--- Grupo_05/modificador_permissoes_atributos.sh
+++ Grupo_05/modificador_permissoes_atributos.sh
@@ -2,8 +2,21 @@
 
 #################################################################################################
 #Script Name	: Modificador de Permissões e Atributos Avançadas				#
-#Description	: 										#
-#Args		: Arquivo a ser verificado ou alterado						#
+#Description	: Realizar alterações na permissões e atributos de arquivos			#
+#Args		: 										#
 #Author		: Mateus Mota Mercẽs, Valesca Melo de Oliveira e Lucas Ferreira Prazere		#
 #Email		: mateusmota0100@gmail.com, .. , .. #						#
 #################################################################################################
+
+Principal() {
+	echo "------------------------------------------------"
+	echo "Script para Modificação de Permissões e Atributos"
+	echo "------------------------------------------------"
+	echo -e " Opções: \n"
+	echo -e "1) Modificar Permissão\n2) Modificar Atributo\n3) Help Stick Bits\n4) Help Chatrir\n5) Sair do script"
+	
+	read -p "Escolha uma opção: " opcao
+
+} 
+
+Principal
