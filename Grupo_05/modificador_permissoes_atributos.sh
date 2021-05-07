b8484746ebf51cc67a2fe97ef2c6f6e184d2e6a9
Mateus <mateusmota0100@gmail.com>
2021-05-07 11:23:39 -0300

    Adicionado script inicial do projeto.

---
+++ Grupo_05/modificador_permissoes_atributos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#################################################################################################
+#Script Name	: Modificador de Permissões e Atributos Avançadas				#
+#Description	: 										#
+#Args		: Arquivo a ser verificado ou alterado						#
+#Author		: Mateus Mota Mercẽs, Valesca Melo de Oliveira e Lucas Ferreira Prazere		#
+#Email		: mateusmota0100@gmail.com, .. , .. #						#
+#################################################################################################
