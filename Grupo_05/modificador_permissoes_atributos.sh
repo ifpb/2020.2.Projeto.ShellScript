fcf63acfc72e7b9c702a23ed714935186f27321b
mateus <Mateus>
2021-05-11 01:11:12 -0300

    Adicionado interface gráfica ao Menu.

--- Grupo_05/modificador_permissoes_atributos.sh
+++ Grupo_05/modificador_permissoes_atributos.sh
@@ -9,14 +9,41 @@
 #################################################################################################
 
 Principal() {
-	echo "------------------------------------------------"
-	echo "Script para Modificação de Permissões e Atributos"
-	echo "------------------------------------------------"
-	echo -e " Opções: \n"
-	echo -e "1) Modificar Permissão\n2) Modificar Atributo\n3) Help Stick Bits\n4) Help Chatrir\n5) Sair do script"
-	
-	read -p "Escolha uma opção: " opcao
+	zenity	--info \
+		--width=350 \
+		--height=100 \
+		--title="Bem-vindo" \
+		--text="Script para Modificação de Permissões e Atributos" 
+
+}
+
+Menu() {
+	escolha=$(zenity --list	\
+		--width=350 \
+		--height=250 \
+		--title "Script para Modificação de Permissões e Atributos" \
+		--text="Escolha uma das opções abaixo"                      \
+		--radiolist                                                 \
+		--hide-column 2                                             \
+       		--column="Marque" --column="" --column="Opções" false 0 "Modificar Permissão" false 1 "Modificar Atributo" false 2 "Help Stick Bits" false 3 "Help Chatrir")				
+
+case $escolha in
+	0)
+		#Implementar funcionalidade para Modificar Permissão.
+		arquivo="$(zenity --file-selection --title="Selecione o arquivo")"
+		;;
+	1)
+		#Implementar funcionalidade para Modificar Permissão.
+		arquivo="$(zenity --file-selection --title="Selecione o arquivo")"
+		;;
+	2)	
+		echo "Você escolheu visualizar o help sobre Stick Bits";;
+	3)
+		echo "Você escolheu visualizr o help sobre Chattr"
+
+esac
 
 } 
 
 Principal
+Menu
