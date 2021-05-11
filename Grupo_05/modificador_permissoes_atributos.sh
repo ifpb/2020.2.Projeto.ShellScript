01d5ce8f52446b838bfb72d677ed852dda018c3f
Mateus <mateusmota0100@gmail.com>
2021-05-11 16:52:23 -0300

    Adicionado as funcionalidades gráficas Menu_Secundario, Menu_Permissoes, Menu_Atributos, Selecionar_Arquivo, Selecionar_Arquivos.

--- Grupo_05/modificador_permissoes_atributos.sh
+++ Grupo_05/modificador_permissoes_atributos.sh
@@ -8,14 +8,18 @@
 #Email		: mateusmota0100@gmail.com, .. , .. #						#
 #################################################################################################
 
+
+
 Inicio() {
 	zenity	--info \
 		--width=350 \
-		--height=100 \
+		--height=150 \
 		--title="Bem-vindo" \
 		--text="Script para Modificação de Permissões e Atributos" 
 
 }
+
+
 
 Menu_Principal() {
 	escolha=$(zenity --list	\
@@ -28,22 +32,53 @@ Menu_Principal() {
        		--column="Marque" --column="" --column="Opções" false 0 "Modificar Permissão" false 1 "Modificar Atributo" false 2 "Help Stick Bits" false 3 "Help Chatrir")				
 
 case $escolha in
-	0)
-		#Implementar funcionalidade para Modificar Permissão.
-		arquivo="$(zenity --file-selection --title="Selecione o arquivo")"
+	0)	
+		opcao="$(Menu_Secundario)"
+		permissoes="$(Menu_Permissoes)"
 		;;
 	1)
-		#Implementar funcionalidade para Modificar Permissão.
-		arquivo="$(zenity --file-selection --title="Selecione o arquivo")"
+		opcao="$(Menu_Secundario)"
+		atributos="$(Menu_Atributos)"
 		;;
 	2)	
 		echo "Você escolheu visualizar o help sobre Stick Bits";;
-	3)
+	3)	
 		echo "Você escolheu visualizr o help sobre Chattr"
 
 esac
 
 } 
+
+
+
+Menu_Secundario() {
+	permissoes=$(zenity --list \
+		--width=350 \
+		--height=250 \
+		--title="" \
+		--text="" \
+		--radiolist \
+		--hide-column=2 \
+		--column="Marque" --column="" --column="Opções" false 0 "Um arquivo" false 1 "Múltiplos Arquivos" false 2 "Diretório")
+
+}
+
+
+Menu_Permissoes() {
+	echo ""	
+}
+
+Menu_Atributos() {
+	echo ""
+}
+
+Selecionar_Arquivo() {
+	arquivo="$(zenity --file-selection --title="Selecione o arquivo")"
+}
+
+Selecionar_Arquivos() {
+	arquivos="$(zenity --file-selection --multiple --title="Selecione os arquivos")"
+}
 
 Inicio
 Menu_Principal
