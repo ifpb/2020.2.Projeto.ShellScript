ab57c5e2d296f4626240de0602789b6bb91467c7
Marcos Ugulino <marcos.ugulino@academico.ifpb.edu.br>
2021-05-12 15:57:48 -0300

    Melhorias no yad.sh

--- Grupo_04/yad.sh
+++ Grupo_04/yad.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Gerenciador de pacotes Snap (Snap-Manager YAD)
+#Gerenciador de pacotes Snap (Snap-Manager YAD)
 #find : mostra snaps que podem ser instalados.
 #info : mostra informações detalhadas sobre os snaps.
 #install : instala snaps no sistema.
@@ -16,31 +16,34 @@
 #funções para trabalhar os comandos do snap
 
 snap_find(){
-	#yad --text="$(snap find)"		#A saída do comando find é muito grande
-	snap find > find_tmp.txt
-	yad --text="$(cat find_tmp.txt)"
+	chave=$(yad --title="Snap - Find" --text="Digite o nome do pacote:" --entry --center)
+	snap find $chave &> /tmp/snap_manager
+	yad --title="Lista de Snaps" --text="$(cat /tmp/snap_manager)"
 }
 
 snap_install(){
-	pacote=$(yad --title="Installer" --text="Digite o nome do pacote:" --entry)
-	snap install $pacote			#a solicitação de de senha está aparecendo no terminal
+	pacote=$(yad --title="Install" --text="Digite o nome do pacote:" --entry --center)
+	sudo snap install $pacote &> /tmp/snap_manager
+	yad --text="$(cat /tmp/snap_manager)" --center 	#a solicitação de senha está aparecendo no terminal
 }
 
 snap_list(){
-	yad --title="Lista de pacotes" --text="$(snap list)"
+	yad --title="Snaps Instalados" --text="$(snap list)" --center
 }
 
 snap_upgrade(){
-	yad --text="$(snap upgrade)"
+	sudo snap refresh &> /tmp/snap_manager
+	yad --text="$(cat /tmp/snap_manager)" --center
 }
 
 snap_remove(){
-	rmv=$(yad --title="Unistaller" --text="Digite o nome do pacote:" --entry)
-	snap remove $rmv			#a solicitação de senha está aparecendo no terminal
+	rmv=$(yad --title="Unistall" --text="Digite o nome do pacote:" --entry --center)
+	sudo snap remove $rmv &> /tmp/snap_manager
+	yad --text="$(cat /tmp/snap_manager)" --center	#a solicitação de senha está aparecendo no terminal
 }
 
 snap_version(){
-	yad --title="Versão do SNAP" --text="$(snap --version)" --width="220"
+	yad --title="Versão do SNAP" --text="$(snap --version)" --width="220" --center
 }
 
 #expotando as funções
@@ -48,11 +51,20 @@ snap_version(){
 export -f snap_find snap_install snap_list snap_upgrade snap_remove snap_version
 
 yad --title="Snap Manager" --center \
-	--text="Escolha a opção desejada:" \
+	--text="						Gerenciador de pacotes Snap (Snap-Manager YAD)
+
+Opções:
+
+find - mostra snaps que podem ser instalados.
+install - instala snaps no sistema.
+list - mostra os snaps instalados.
+upgrade - atualiza todos snaps.
+remove - remove snaps instalados.
+version - mostra informações sobre o programa." \
 	--button="find":"bash -c snap_find" \
 	--button="install":"bash -c snap_install" \
 	--button="list":"bash -c snap_list" \
-	--button="upgrade" \
+	--button="upgrade":"bash -c snap_upgrade" \
 	--button="remove":"bash -c snap_remove" \
 	--button="version":"bash -c snap_version"\
-	--button="Exit":0 --width:"640" --height="140"
+	--button="Exit":0 --width:"640" --height="480"
