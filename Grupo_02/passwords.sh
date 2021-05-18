346971e482ce5419e705dbef143f5c3a21d8a055
Jefferson-LFS <jefferson.altere@gmail.com>
2021-05-18 15:15:03 -0300

    ajuste no forms, regex e verificações

--- Grupo_02/passwords.sh
+++ Grupo_02/passwords.sh
@@ -13,7 +13,7 @@ while true; do
 		01) 
             
            
-			usuario=$(zenity --forms --title="Alterando a senha do Usuário" \
+			usuario=$(zenity --forms --title="Alterando a senha do Usuário" --text="Informe: " \
 				--add-entry="Nome do user: " \
 				--add-password="Nova senha: " \
 				--add-password="Digite novamente: " --width="600" --height="400")
@@ -23,22 +23,29 @@ while true; do
 					passw1=$(echo $usuario | cut -d'|' -f2)
 					passw2=$(echo $usuario | cut -d'|' -f3)
 					if [ -n "$user" ];then
-						if [ "${passw1}" == "${passw2}" ] && echo $passw1 | grep -P "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[[:punct:]]){8,}" &>/dev/null;then
+						if [ "${passw1}" == "${passw2}" ] && echo $passw1 | grep -P "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[[:punct:]]){6,}" &>/dev/null;then
 							echo $usuario | cut -d'|' -f2,3 | tr -s '|' '\n' > /tmp/.passwd_1.txt
 							$(passwd $user < /tmp/.passwd_1.txt &> /dev/null)
-							$(zenity --info --title="System" --text="Senha alterada Com Sucesso!" --width="600" --height="400")
-
+							if [ $? -eq 0];then
+								$(zenity --info --title="System" --text="Senha Alterada com Sucesso!" --width="600" --height="400")
+							else
+								$(zenity --error --title="System" --text="Falha ao Alterar Senha!" --width="600" --height="400")
+							fi
 						else
 							$(zenity --error --text="Senhas diferentes! \nSenha precisa conter ao menos 6 caracteres (Aa1@)" --width="600" --height="400")
-							senha=$(zenity --forms --title="Digte ambas as senhas iguais!" \
+							senha=$(zenity --forms --title="Digte ambas as senhas iguais!" --text="Informe: " \
 								--add-password="Senha " \
 								--add-password="Novamente " --width="600" --height="400")
 							passw1=$(echo $senha | cut -d'|' -f1)
 							passw2=$(echo $senha | cut -d'|' -f2)
-							if [ "${passw1}" == "${passw2}" ] && echo $passw1 | grep -E '\b(([A-Za-z0-9]+)|([[:punct:]]+)){6}\b' &> /dev/null;then
+							if [ "${passw1}" == "${passw2}" ] && echo $passw1 | grep -E '\b(([A-Za-z0-9]+)|([[:punct:]]+)){6,}\b' &> /dev/null;then
 								echo $usuario | cut -d'|' -f2,3 | tr -s '|' '\n' > /tmp/.passwd_1.txt
 							    $(passwd $user < /tmp/.passwd_1.txt &> /dev/null)
-								$(zenity --info --title="System" --text="Senha alterada Com Sucesso!" --width="600" --height="400")
+								if [ $? -eq 0];then
+									$(zenity --info --title="System" --text="Senha alterada Com Sucesso!" --width="600" --height="400")
+								else
+									$(zenity --error --title="System" --text="Falha ao Alterar Senha!" --width="600" --height="400")
+								fi
 							else
 								$(zenity --error --text="Não foi possivel alterar senha!\nTente novamente!" --width="600" --height="400")
 							fi
@@ -49,7 +56,7 @@ while true; do
 					;;
 
 				1)
-					$(zenity --info --title="System" --text="Alteração de usuário cancelada !" --width="600" --height="400")
+					$(zenity --info --title="System" --text="Alteração de usuário cancelada!" --width="600" --height="400")
 			esac
 
 			;;
