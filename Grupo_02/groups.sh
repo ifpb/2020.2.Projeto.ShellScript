fed3ca1019c2326cc83fdba078041fdfca911c24
Diegofigueiredo1 <diegofigueiredo93@gmail.com>
2021-05-18 13:45:43 -0300

    Add 2 novas opções

--- Grupo_02/groups.sh
+++ Grupo_02/groups.sh
@@ -10,7 +10,9 @@ while true; do
 		"03" "Adicionar Grupo ao Usuário" \
 		"04" "Remover Grupo do Usuário" \
 		"05" "Alterar Grupo de uma Arquivo ou Diretório" \
-		"06" "Exit" --width="600" --height="400")
+		"06" "Listar Grupos" \
+		"07" "Listar Grupos do Usuário" \
+		"08" "Exit" --width="600" --height="400")
 	opcao1=$(echo $opcao)
 
 	case $opcao1 in
@@ -148,8 +150,41 @@ while true; do
 			esac
 						
 			;;
-
 		06)
+			$(awk -F":" '{if( $3 >= 1000 && $3  <= 29999) print ($3" "$1)}' /etc/group > /tmp/.group_list.txt)
+			$(zenity --info --title="Lista de Grupos" --text="$(awk 'BEGIN{print "ID Grupos"}{print $1" "$2 }' /tmp/.group_list.txt)" --width="400" --height="400")
+			
+			;;
+		07)
+			listgrupo=$(zenity --forms --title="Lista de Grupos do Usuário" --text="Informe: " \
+				--add-entry="Nome do Usuário " --width="600" --height="400")
+			case $? in
+				0)
+					if [ -n "$listgrupo" ];then
+						$(groups ${listgrupo} &>/dev/null)
+						if [ $? -eq 0 ];then
+							$(groups ${listgrupo} | tr -s ' ' '\n' | awk '{if (NR >=3) print $1}' > /tmp/.group_user.txt)
+							$(zenity --info --title="Lista" --text="$(cat /tmp/.group_user.txt | awk 'BEGIN{print "Grupo(s) do Usuário: ""'$listgrupo'\n" }{print $1}')" --width="600" --height="400")
+						else
+							$(zenity --error --title="System" --text="Falha ao listar grupos do usuário!" --width="600" --height="400")
+						fi
+					else
+						$(zenity --info --title="System" --text="Usuário não informado!" --width="600" --height="400")
+
+					fi
+
+					;;
+				1)
+					$(zenity --info --title="System" --text="Usuário não informado!" --width="600" --height="400")
+
+					;;
+			esac
+
+
+			;;
+
+
+		08)
 			$(zenity --info --title="System" --text="Saindo do Menu de Grupos" --width="600" --height="400")
 			break		
 
