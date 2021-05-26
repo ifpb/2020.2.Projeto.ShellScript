cd63d1e286210e0090b38907bf1ec5f3ee834e59
Diegofigueiredo1 <diegofigueiredo93@gmail.com>
2021-05-26 17:39:28 -0300

    Correções de erros

--- Grupo_02/Senha/passwordmenu.sh
+++ Grupo_02/Senha/passwordmenu.sh
@@ -2,14 +2,14 @@
 
 while true;
 do
-	$(yad --form --image ~/2020.2.Projeto.ShellScript/Grupo_02/Imagens/password.png --image-on-top \
+	$(yad --form --image ./Imagens/password.png --image-on-top \
 		--title="Administração de Sistemas" --center --text="<b>Menu</b>\n<i>Senha</i>" --text-align=center \
 		--expander="Opções" \
-		--field="Trocar":FBTN "bash -c ~/2020.2.Projeto.ShellScript/Grupo_02/Senha/./trocar_pass.sh" \
-		--field="Verificar E":FBTN "@bash -c ~/2020.2.Projeto.ShellScript/Grupo_02/Senha/./exp_pass.sh" \
-		--field="Bloquear":FBTN "@bash -c ~/2020.2.Projeto.ShellScript/Grupo_02/Senha/./bloq_pass.sh" \
-		--field="Desbloquear":FBTN "@bash -c ~/2020.2.Projeto.ShellScript/Grupo_02/Senha/./desbloq_pass.sh" \
-		--field="Alterar":FBTN "@bash -c ~/2020.2.Projeto.ShellScript/Grupo_02/Senha/./alt_pass.sh" \
+		--field="Trocar":FBTN "bash -c ./Senha/trocar_pass.sh" \
+		--field="Verificar E":FBTN "@bash -c ./Senha/exp_pass.sh" \
+		--field="Bloquear":FBTN "@bash -c ./Senha/bloq_pass.sh" \
+		--field="Desbloquear":FBTN "@bash -c ./Senha/desbloq_pass.sh" \
+		--field="Alterar":FBTN "@bash -c ./Senha/alt_pass.sh" \
 		--columns=2 --borders=2 \
 		--button="Voltar":1  --buttons-layout=center \
 	       	--width="500" --height="400")
--- Grupo_02/setupyad.sh
+++ Grupo_02/setupyad.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+
 while true; do
-	$(yad --form --image ~/2020.2.Projeto.ShellScript/Grupo_02/Imagens/flamengo.png --image-on-top \
+	$(yad --form --image ./Imagens/flamengo.png --image-on-top \
 		--title="Administração de Sistemas" --center --text="<b>Bem vindo</b> <i>$(whoami)</i>" --text-align=center \
 		--expander="Opções" \
-		--field="Usuário":FBTN "@bash -c ~/2020.2.Projeto.ShellScript/Grupo_02/Usuario/usermenu.sh" \
-		--field="Grupo":FBTN "@bash -c ~/2020.2.Projeto.ShellScript/Grupo_02/Grupo/./groupmenu.sh" \
-		--field="Senha":FBTN "@bash -c ~/2020.2.Projeto.ShellScript/Grupo_02/Senha/./passwordmenu.sh" \
+		--field="Usuário":FBTN "@bash -c ./Usuario/usermenu.sh" \
+		--field="Grupo":FBTN "@bash -c ./Grupo/groupmenu.sh" \
+		--field="Senha":FBTN "@bash -c ./Senha/passwordmenu.sh" \
 		--columns=1 --borders=2 \
 		--button="gtk-stop":1  --buttons-layout=center \
 	       	--width="500" --height="400")
