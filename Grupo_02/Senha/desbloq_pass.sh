25c7b37e475e136d5c02849a90b8ed846a4de0b0
renato-fernandes-alves <61331075+renato-fernandes-alves@users.noreply.github.com>
2021-05-26 20:59:04 -0300

    Update desbloq_pass.sh

--- Grupo_02/Senha/desbloq_pass.sh
+++ Grupo_02/Senha/desbloq_pass.sh
@@ -5,17 +5,17 @@ user=$(zenity --forms --title="Desbloquear a Conta" --text="Informe: " \
 					if [ -n "$user" ];then
 						$(passwd -u $user &>/dev/null)
 						if [ $? -eq 0 ];then
-							$(zenity --info --title="System" --text="Conta Desbloqueada!" --width="600" --height="400")
+							$(yad --title="System" --center --image ./Imagens/sucess.png --image-on-top --text="<b>Executado com Sucesso</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
 						else
-							$(zenity --error --title="System" --text="Falha ao Desbloquear Conta!" --width="600" --height="400")
+							$(yad --title="System" --center --image ./Imagens/info.png --image-on-top --text="<b>Falha ao Executar</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
 						fi
 					else
-						$(zenity --info --title="System" --text="Conta não Informada!" --width="600" --height="400")
+						$(yad --title="System" --center --image ./Imagens/error.png --image-on-top --text="<b>ERROR - Parâmetro não Encontrado</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
 					
 					fi
 					;;
 				1)
-					$(zenity --info --title="System" --text="Conta não Informada!" --width="600" --height="400")
+					$(yad --title="System" --center --image ./Imagens/error.png --image-on-top --text="<b>ERROR - Parâmetro não Encontrado</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
 					;;
 			esac
 
