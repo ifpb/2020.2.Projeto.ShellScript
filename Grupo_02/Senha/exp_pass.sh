774cc8cde024952fabccbf10349bd5584b87dd64
renato-fernandes-alves <61331075+renato-fernandes-alves@users.noreply.github.com>
2021-05-26 21:00:03 -0300

    Update exp_pass.sh

--- Grupo_02/Senha/exp_pass.sh
+++ Grupo_02/Senha/exp_pass.sh
@@ -5,11 +5,12 @@ user=$(zenity --forms --title="Exibir Data de Expiração da Senha" --text="Info
 					if [ -n "$user" ];then
 						$(zenity --info --title="Data" --text="$(chage -l xico | tr -d '[\t]' | awk -F':' 'BEGIN {print "Data de Expiração: \n"}{if(NR >=2 && NR <=2) print $2}')" --width="600" --height="400" )
 					else
-						$(zenity --error --title="System" --text="Falha ao exibir data de expiração!" --width="600" --height="400")
+						$(yad --title="System" --center --image ./Imagens/info.png --image-on-top --text="<b>Falha ao Executar</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
+
 					fi
 					;;
 				1)
-					$(zenity --info --title="System" --text="Usuário não informado!" --width="600" --height="400")
+					$(yad --title="System" --center --image ./Imagens/error.png --image-on-top --text="<b>ERROR - Parâmetro não Encontrado</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
 					;;
 			esac
 
