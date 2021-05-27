6b4f572706f1ab61324c8bdb5a5f51bb95581ea7
renato-fernandes-alves <61331075+renato-fernandes-alves@users.noreply.github.com>
2021-05-26 21:58:07 -0300

    Update criar_group.sh

--- Grupo_02/Grupo/criar_group.sh
+++ Grupo_02/Grupo/criar_group.sh
@@ -7,17 +7,17 @@ grupo=$(zenity --forms --title="Criando Grupo " --text="Informe: " \
 					if [ -n "$grupo" ]; then
 						$(addgroup $grupo &>/dev/null)
 						if [ $? -eq 0 ];then
-							$(zenity --info --title="System" --text="Grupo Criado Com Sucesso!" --width="600" --height="400")
+							$(yad --title="System" --center --image ./Imagens/sucess.png --image-on-top --text="<b>Executado com Sucesso</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
 						else
-							$(zenity --error --title="System" --text="Falha Na Criação Do Grupo!" --width="600" --height="400")
+							$(yad --title="System" --center --image ./Imagens/info.png --image-on-top --text="<b>Falha ao Executar</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
 						fi
 					else
-						$(zenity --info --title="System" --text="Nenhum Grupo Informado!" --width="600" --height="400")
+						$(yad --title="System" --center --image ./Imagens/error.png --image-on-top --text="<b>ERROR - Parâmetro não Encontrado</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
 					fi
 
 					;;
 				1)
-					$(zenity --info --title="System" --text="Nenhum Grupo Informado!" --width="600" --height="400")
+					$(yad --title="System" --center --image ./Imagens/error.png --image-on-top --text="<b>ERROR - Parâmetro não Encontrado</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
 					;;
 
 			esac
