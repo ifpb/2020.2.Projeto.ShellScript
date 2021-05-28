52bed6a62891802430b3b92f824715eed5badbf5
renato-fernandes-alves <61331075+renato-fernandes-alves@users.noreply.github.com>
2021-05-28 09:01:23 -0300

    Update trocar_pass.sh

--- Grupo_02/Senha/trocar_pass.sh
+++ Grupo_02/Senha/trocar_pass.sh
@@ -43,6 +43,6 @@ usuario=$(zenity --forms --title="Alterando a senha do Usuário" --text="Informe
 					;;
 
 				1)
-					$(yad --title="System" --center --image ./Imagens/error.png --image-on-top --text="<b>ERROR - Parâmetro não Encontrado</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
+				        ;; 
 			esac
 
