824b9788dca0c20325c0b63cab1a3ae65b70858e
renato-fernandes-alves <61331075+renato-fernandes-alves@users.noreply.github.com>
2021-05-28 09:04:52 -0300

    Update alt_pass.sh

--- Grupo_02/Senha/alt_pass.sh
+++ Grupo_02/Senha/alt_pass.sh
@@ -17,7 +17,6 @@ num=$(zenity --forms --title="Alteração da Data de Expiração" --text="Inform
 					fi
 					;;
 				1)
-					$(yad --title="System" --center --image ./Imagens/error.png --image-on-top --text="<b>ERROR - Parâmetro não Encontrado</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
 					;;
 			esac
 
