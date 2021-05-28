438ed9b33560040c9a9ffc81a2182de54e69836c
renato-fernandes-alves <61331075+renato-fernandes-alves@users.noreply.github.com>
2021-05-28 09:02:49 -0300

    Update desbloq_pass.sh

--- Grupo_02/Senha/desbloq_pass.sh
+++ Grupo_02/Senha/desbloq_pass.sh
@@ -15,7 +15,6 @@ user=$(zenity --forms --title="Desbloquear a Conta" --text="Informe: " \
 					fi
 					;;
 				1)
-					$(yad --title="System" --center --image ./Imagens/error.png --image-on-top --text="<b>ERROR - Parâmetro não Encontrado</b>" --text-align=center --button="gtk-ok":0 --buttons-layout=center --width="500" --height="400")
 					;;
 			esac
 
