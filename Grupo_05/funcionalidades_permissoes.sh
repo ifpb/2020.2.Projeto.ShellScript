c3304b8c4e172363638328bb9d874c5594aac959
Valesca Melo <valesca.melo@academico.ifpb.edu.br>
2021-05-29 21:33:46 -0300

    Adicionando operações de leitura, gravação e execução. Juntos das especificações das classes dos usuários.

--- Grupo_05/funcionalidades_permissoes.sh
+++ Grupo_05/funcionalidades_permissoes.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
-adicionar-execucao(){
-	chmod +x $arquivo 
+#Adicionando leitura para dono, grupo, outros e todos.
+
+adicionar-leitura-dono-arquivo(){
+	chmod +ru $arquivo 
+}
+adicionar-leitura-grupo-arquivo(){
+	chmod +rg $arquivo
+}
+adicionar-leitura-outros(){
+	chmod +ro $arquivo
+}
+adicionar-leitura-todos(){
+	chmod +ra $arquivo
+}
+
+#Adicionando escrita para dono, grupo, outros e todos.
+
+adicionar-escrita-dono-arquivo(){
+	chmod +wu $arquivo
+}
+adicionar-escrita-grupo-arquivo(){
+	chmod +wg $arquivo
+}
+adicionar-escrita-outros(){
+	chmod +wo $arquivo
+}
+adicionar-escrita-todos(){
+	chmod +wa
+}
+
+#Adicionando execução para dono, grupo, outros e todos.
+
+adicionar-execucao-dono-arquivo(){
+	chmod +xu $arquivo
+}
+adicionar-execucao-grupo-arquivo(){
+	chmod +xg $arquivo
+}
+adicionar-execucao-outros(){
+	chmod +xo $arquivo
+}
+adicionar-execucao-todos(){
+	chmod +xa
 }
