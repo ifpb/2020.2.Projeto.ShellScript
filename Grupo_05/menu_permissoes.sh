b444c9d20f420f1b2025e3260887f1228b8101b9
Mateus_Mota <47959481+Mateus-Mota@users.noreply.github.com>
2021-05-30 19:07:19 -0300

    Update menu_permissoes.sh
    Adicionado o Menu de Permissões que estava no arquivo Menus_graficos.sh

--- Grupo_05/menu_permissoes.sh
+++ Grupo_05/menu_permissoes.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+
+source menus_graficos.sh
+
+Menu_permissoes() {
+		
+    modo_arquivo="$(Menu_Tipo_Arquivo)"
+		
+		
+		if [ "$modo_arquivo" == "0" ]; then
+			arquivo="$(Selecionar_Arquivo)"
+			operador="$(Menu_Operador)"
+			usuarios="$(Menu_Usuarios)"
+			permissoes="$(Menu_Permissoes)"	
+
+			if [[ $permissoes =~ 0 && "$operador" == "0" ]]; then
+				adicionar-execucao $arquivo
+			fi
+		fi
+		
+		if [ "$modo_arquivo" == "1" ]; then
+			arquivos="$(Selecionar_Arquivos)"
+			usuarios="$(Menu_Usuarios)"
+			permissoes="$(Menu_Permissoes)"
+			operador="$(Menu_Operador)"	
+		fi
+	
+		if [ "$modo_arquivo" == "2" ]; then 
+			diretorio="$(Selecionar_Diretorio)"
+			usuarios="$(Menu_Usuarios)"
+			permissoes="$(Menu_Permissoes)"
+			operador="$(Menu_Operador)"
+		fi
+
+}
