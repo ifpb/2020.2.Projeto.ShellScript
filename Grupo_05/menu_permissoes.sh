9838af5c981905a425982607c8bf53267b1bba60
Mateus <mateusmota0100@gmail.com>
2021-05-31 12:29:41 -0300

    Alterações na lógica das condições.

--- Grupo_05/menu_permissoes.sh
+++ Grupo_05/menu_permissoes.sh
@@ -15,54 +15,60 @@ Menu_Permissoes() {
 				
 			#Condições para Adição de Permissões
 			#Condições de Execução
-			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "0" ]]; then
-				adicionar-execucao-dono-arquivo $arquivo
-			fi
+			if [[ $permissoes =~0 ]]; then
+				if [[ "$operador" == "0" && "$usuarios" == "0" ]]; then
+					adicionar-execucao-dono-arquivo $arquivo
+				fi
 			
-			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "1" ]]; then
-				adicionar-execucao-grupo-arquivo $arquivo
-			fi
+				if [[ "$operador" == "0" && "$usuarios" == "1" ]]; then
+					adicionar-execucao-grupo-arquivo $arquivo
+				fi
 			
-			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "2" ]]; then
-				adicionar-execucao-outros $arquivo
-			fi
+				if [[ "$operador" == "0" && "$usuarios" == "2" ]]; then
+					adicionar-execucao-outros $arquivo
+				fi
 			
-			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "3" ]]; then
-				adicionar-execucao-todos $arquivo
-			fi	
+				if [[ "$operador" == "0" && "$usuarios" == "3" ]]; then
+					adicionar-execucao-todos $arquivo
+				fi
+			fi
 			
 			#Condições para Escrita
-			if [[ $permissoes =~ 1 && "$operador" == "1" && "$usuarios" == "0" ]]; then
-				adicionar-escrita-dono-arquivo $arquivo
-			fi
+			if [[ $permissoes =~ 1 ]]; then
+				if [[ "$operador" == "1" && "$usuarios" == "0" ]]; then
+					adicionar-escrita-dono-arquivo $arquivo
+				fi
 			
-			if [[ $permissoes =~ 1 && "$operador" == "1" && "$usuarios" == "1" ]]; then
-				adicionar-escrita-grupo-arquivo $arquivo
-			fi
+				if [[ $permissoes =~ 1 && "$operador" == "1" && "$usuarios" == "1" ]]; then
+					adicionar-escrita-grupo-arquivo $arquivo
+				fi
 			
-			if [[ $permissoes =~ 1 && "$operador" == "1" && "$usuarios" == "2" ]]; then
-				adicionar-escrita-outros $arquivo
-			fi
+				if [[ $permissoes =~ 1 && "$operador" == "1" && "$usuarios" == "2" ]]; then
+					adicionar-escrita-outros $arquivo
+				fi
 			
-			if [[ $permissoes =~ 1 && "$operador" == "1" && "$usuarios" == "3" ]]; then
-				adicionar-escrita-todos $arquivo
+				if [[ $permissoes =~ 1 && "$operador" == "1" && "$usuarios" == "3" ]]; then
+					adicionar-escrita-todos $arquivo
+				fi
 			fi	
 			
 			#Condições para Leitura
-			if [[ $permissoes =~ 2 && "$operador" == "1" && "$usuarios" == "0" ]]; then
-				adicionar-leitura-dono-arquivo $arquivo
-			fi
+			if [[ $permissoes =~2 ]]; then
+				if [[ $permissoes =~ 2 && "$operador" == "1" && "$usuarios" == "0" ]]; then
+					adicionar-leitura-dono-arquivo $arquivo
+				fi
 			
-			if [[ $permissoes =~ 2 && "$operador" == "1" && "$usuarios" == "1" ]]; then
-				adicionar-leitura-grupo-arquivo $arquivo
-			fi
+				if [[ $permissoes =~ 2 && "$operador" == "1" && "$usuarios" == "1" ]]; then
+					adicionar-leitura-grupo-arquivo $arquivo
+				fi
 			
-			if [[ $permissoes =~ 2 && "$operador" == "1" && "$usuarios" == "2" ]]; then
-				adicionar-leitura-outros $arquivo
-			fi
+				if [[ $permissoes =~ 2 && "$operador" == "1" && "$usuarios" == "2" ]]; then
+					adicionar-leitura-outros $arquivo
+				fi
 			
-			if [[ $permissoes =~ 2 && "$operador" == "1" && "$usuarios" == "3" ]]; then
-				adicionar-leitura-todos $arquivo
+				if [[ $permissoes =~ 2 && "$operador" == "1" && "$usuarios" == "3" ]]; then
+					adicionar-leitura-todos $arquivo
+				fi
 			fi
 			
 			
