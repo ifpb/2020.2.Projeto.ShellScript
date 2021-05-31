4f3e257d8982f525542387272dd557e58c0434d0
Mateus_Mota <47959481+Mateus-Mota@users.noreply.github.com>
2021-05-30 21:37:02 -0300

    Update menu_permissoes.sh
    Adicionado Condições para adicionar permissões de execução em um único arquivo.

--- Grupo_05/menu_permissoes.sh
+++ Grupo_05/menu_permissoes.sh
@@ -13,10 +13,34 @@ Menu_Permissoes() {
 			operador="$(Menu_Selecionar_Operador)"
 			usuarios="$(Menu_Selecionar_Usuarios)"
 				
-
-			if [[ $permissoes =~ 0 && "$operador" == "0" ]]; then
-				adicionar-execucao $arquivo
+			#Condições para Adição de Permissões
+			#Condições de Execução
+			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "0"]]; then
+				adicionar-execucao-dono-arquivo $arquivo
 			fi
+			
+			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "1"]]; then
+				adicionar-execucao-grupo-arquivo $arquivo
+			fi
+			
+			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "2"]]; then
+				adicionar-execucao-outros $arquivo
+			fi
+			
+			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "3"]]; then
+				adicionar-execucao-todos $arquivo
+			fi	
+			
+			#Condições para Escrita
+			
+			#Condições para Leitura
+			
+			
+			#Condições para Remoção de Permissões
+			#Condições de Execução
+			#Condições para Escrita
+			#Condições para Leitura
+			
 		fi
 		
 		if [ "$modo_arquivo" == "1" ]; then
