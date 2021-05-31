b30e9bdb6d27cda7e502166b965a4ef55bb3e14f
Mateus_Mota <47959481+Mateus-Mota@users.noreply.github.com>
2021-05-30 22:51:01 -0300

    Update menu_permissoes.sh
    Alterações na sintaxe

--- Grupo_05/menu_permissoes.sh
+++ Grupo_05/menu_permissoes.sh
@@ -15,19 +15,19 @@ Menu_Permissoes() {
 				
 			#Condições para Adição de Permissões
 			#Condições de Execução
-			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "0"]]; then
+			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "0" ]]; then
 				adicionar-execucao-dono-arquivo $arquivo
 			fi
 			
-			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "1"]]; then
+			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "1" ]]; then
 				adicionar-execucao-grupo-arquivo $arquivo
 			fi
 			
-			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "2"]]; then
+			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "2" ]]; then
 				adicionar-execucao-outros $arquivo
 			fi
 			
-			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "3"]]; then
+			if [[ $permissoes =~ 0 && "$operador" == "0" && "$usuarios" == "3" ]]; then
 				adicionar-execucao-todos $arquivo
 			fi	
 			
