b8495193cc6a87acb8c489b0d5f4c08c9691b264
Mateus <mateusmota0100@gmail.com>
2021-06-01 18:18:06 -0300

    Alterações na largura e altura da interface.

--- Grupo_05/funcionalidades_help.sh
+++ Grupo_05/funcionalidades_help.sh
@@ -2,11 +2,15 @@
 
 Help_Sticky_Bit() {
 	zenity  --text-info \
-		--filename=help_sticky_bit.txt
+		--filename=help_sticky_bit.txt \
+		--width=450 \
+		--height=350 
 
 }
 
 Help_Chattr() {
 	zenity 	--text-info \
 		--filname=help_chattr.txt
+		--width=450
+		--height=350
 }
