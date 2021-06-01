339d0e259679a78ebdc7cb73967ecfa0e119794e
Mateus_Mota <47959481+Mateus-Mota@users.noreply.github.com>
2021-05-31 23:48:19 -0300

    Update menu_permissoes.sh
    Adicionado condições para remoção de permissões de execução, escrita e leitura no modo de arquivo único.

--- Grupo_05/menu_permissoes.sh
+++ Grupo_05/menu_permissoes.sh
@@ -61,7 +61,7 @@ Menu_Permissoes() {
 
 				#Condições para Leitura
 				if [[ $permissoes =~ 2 && "$verifica_leitura" == "0" ]]; then
-					if [[ "$operador" == "1" && "$usuarios" == "0" ]]; then
+					if [[ "$operador" == "0" && "$usuarios" == "0" ]]; then
 						adicionar-leitura-dono-arquivo $arquivo
 					fi
 
@@ -81,9 +81,68 @@ Menu_Permissoes() {
 			done
 			
 			#Condições para Remoção de Permissões
-			#Condições de Execução
-			#Condições para Escrita
-			#Condições para Leitura
+			for (( i=0; i<${#permissoes}; i++ )); do
+				#Condições para Remoção de Permissões
+				#Condições de Execução
+				if [[ $permissoes =~ 0 && "$verifica_execucao" == "0" ]]; then
+					if [[ "$operador" == "1" && "$usuarios" == "0" ]]; then
+						remover-execucao-dono-arquivo $arquivo
+					fi
+
+					if [[ "$operador" == "1" && "$usuarios" == "1" ]]; then
+						remover-execucao-grupo-arquivo $arquivo
+					fi
+
+					if [[ "$operador" == "1" && "$usuarios" == "2" ]]; then
+						remover-execucao-outros $arquivo
+					fi
+
+					if [[ "$operador" == "1" && "$usuarios" == "3" ]]; then
+						remover-execucao-todos $arquivo
+					fi
+					verifica_execucao="1"
+				fi
+
+				#Condições para Escrita
+				if [[ $permissoes =~ 1 && "$verifica_escrita" == "0" ]]; then
+					if [[ "$operador" == "1" && "$usuarios" == "0" ]]; then
+						remover-escrita-dono-arquivo $arquivo
+					fi
+
+					if [[ "$operador" == "1" && "$usuarios" == "1" ]]; then
+						remover-escrita-grupo-arquivo $arquivo
+					fi
+
+					if [[ "$operador" == "1" && "$usuarios" == "2" ]]; then
+						remover-escrita-outros $arquivo
+					fi
+
+					if [[ "$operador" == "1" && "$usuarios" == "3" ]]; then
+						remover-escrita-todos $arquivo
+					fi
+					verifica_escrita="1"
+				fi	
+
+				#Condições para Leitura
+				if [[ $permissoes =~ 2 && "$verifica_leitura" == "0" ]]; then
+					if [[ "$operador" == "1" && "$usuarios" == "0" ]]; then
+						remover-leitura-dono-arquivo $arquivo
+					fi
+
+					if [[ "$operador" == "1" && "$usuarios" == "1" ]]; then
+						remover-leitura-grupo-arquivo $arquivo
+					fi
+
+					if [[ "$operador" == "1" && "$usuarios" == "2" ]]; then
+						remover-leitura-outros $arquivo
+					fi
+
+					if [[ "$operador" == "1" && "$usuarios" == "3" ]]; then
+						remover-leitura-todos $arquivo
+					fi
+					verifica_leitura="1"
+				fi
+			done
 			
 		fi
 		
