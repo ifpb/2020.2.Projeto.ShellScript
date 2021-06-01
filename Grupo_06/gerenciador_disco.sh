a570eb55d69354505259357992887e119e6b7ce6
Moroni Santos <61150397+Moronists@users.noreply.github.com>
2021-06-01 12:18:10 -0300

    Incorporado Zenity no código;

--- Grupo_06/gerenciador_disco.sh
+++ Grupo_06/gerenciador_disco.sh
@@ -1,114 +1,109 @@
 #!/bin/bash
 
-echo "O programa necessita de permissão elevada para ser executado."
+while true
+do
 
-[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
-
-clear
-Menu() {
-
-	echo " ---------------------------------"
-	echo " Gerenciador de DISCO "
-	echo "----------------------------------"
-	echo
-	echo "[ 1 ] mostrar os discos em uso "
-	echo "[ 2 ] listar as partições "
-	echo "[ 3 ] montar partição "
-	echo "[ 4 ] desmontar partição " 
-	echo "[ 5 ] compartilhamento samba "
-	echo "[ 6 ] sair "
-	echo
-	echo -n "Qual a opção desejada ? "
-	read opcao
-	case $opcao in
-		1) mostrar ;;
-		2) listar ;;
-		3) montar ;;
-		4) desmontar ;; 
-		5) compartilhar ;;
-		6) exit ;;
-		*) echo "Opção desconhecida"; 
-		    echo; Menu
-	esac
-}
-
-mostrar() {
+opcao=$(zenity --width=280 --height=250 --list --title="Gerenciador de Discos" --column="Id" --column="Opção" 1 "mostrar os discos em uso" \
+2 "listar as partições" \
+3 "montar partição" \
+4 "desmontar partição" \
+5 "compartilhamento samba" \
+6 "sair do programa" )
 
 
-        echo -e "\n mostrar os discos em uso  "
-        df -h
-	Menu
-}
+[[ "$?" != "0" ]] && exit 1
 
-listar() {
-
-       echo -e " \n listar partições " 
-       lsblk
-       Menu
-}
-
-montar() {
-
-	echo -e "\n  montar Partição : " 
-
-	echo -e  "\n  Informe o nome desejado para pasta a ser ponto de montagem: "
-	read mntpoint
+if [ $opcao -eq 1 ]; then
 	
-	echo -e  "\n  Informe o dispositivo especial a ser montado na pasta(sda, sdb...): "
-	read mntdev
+	df -h > discos.txt
+	zenity --width=576 --height=400 --list --title="Discos em Uso"	--column="Sis. Arq" --column="Tam." --column="Usado"  	--column="Disp." --column="Uso%" --column="Montado em" \
+	$(tail -n +2 discos.txt)
+	[[ "$?" != "0" ]] && exit 1
+
+elif [ $opcao -eq 2 ]; then
+        
+        lsblk > part.txt
+	zenity --width=576 --height=400 --list --title="Partições" --column="Nome" --column="MAJ:MIN" --column="RM" --column="Tamanho" --column="RO" --column="Tipo" --column="Ponto de Montagem" \
+  	$(tail -n +2 part.txt)
+  	[[ "$?" != "0" ]] && exit 1
+  	
+elif [ $opcao -eq 3 ]; then 
+	
+	lsblk --noheadings --raw -o NAME,MOUNTPOINT | awk '$1~/[[:digit:]]/ && $2 == ""' > unmnt.txt
+	
 		
-	mntdir="/media/$mntpoint"
+	if [ ! -d ~/shmount ]; then
 	
-	cd /media/
+		sudo -k mkdir ~/shmount 
 	
-	if [ ! -d "$mntdir" ]; then
-	mkdir /media/$mntpoint/
-	mount /dev/$mntdev /media/$mntpoint/
-	df -h
+		mntdev=$(zenity --list --title="Discos em Uso" --column="Sis. Arq" \
+		$(cat unmnt.txt))
+		
+		[[ "$?" != "0" ]] && exit 1           
 	
+		echo $mntdev > mntdev.txt
+		sudo -k mount /dev/$(cat mntdev.txt) ~/shmount
+		zenity --width=225 --height=100 --info \
+		--text="Disco montado com sucesso."
+		
 	else
-	
-	mount /dev/$mntdev /media/$mntpoint/
-	df -h
+	         
+		mntdev=$(zenity --list --title="Discos em Uso" --column="Sis. Arq" \
+		$(cat unmnt.txt))           
+		
+		[[ "$?" != "0" ]] && exit 1
+		
+		echo $mntdev > mntdev.txt
+		sudo -k mount /dev/$(cat mntdev.txt) ~/shmount
+		zenity --width=225 --height=100 --info \
+		--text="Disco montado com sucesso."
 	
 	fi
-	Menu
-}
+           
 
 
-desmontar() {
+elif [ $opcao -eq 4 ]; then 
+        
+        df -h > discos.txt     
+	mntdev=$(zenity --width=576 --height=400 --list --title="Desmontar disco" --column="Sis. Arq" --column="Tam." --column="Usado" 	--column="Disp." --column="Uso%" --column="Montado em" \
+  	$(tail -n +2 discos.txt))           
 	
+	[[ "$?" != "0" ]] && exit 1	
 	
-	echo -e "\n diretórios usados para montagem:\n "
-	cd /media/
-	ls -l 	
-	echo -e "\n desmontar qual diretório? \n "
-	read mntpoint
-	umount /media/$mntpoint/
+	echo $mntdev > mntdev.txt
+	sudo -k umount $(cat mntdev.txt)
 	
-}
+	zenity --width=225 --height=100 --info \
+	--text="Disco desmontado com sucesso."
+		     
+	     	
+elif [ $opcao -eq 5 ]; then
 
-
-
-compartilhar() {
+	zenity --width=225 --height=100 --info \
+	--text="Iniciando configuração de servidor SMB."	
 	#instalação e configuração do samba
-	apt install samba # instalando o samba
-	/etc/init.d/smbd restart # reinicializando o samba
-	mkdir compSamba # criando a pasta para ser  compartilhada 
-	chmod -R 777 compSamba #alterando as permissões
-	echo "
+	sudo apt install libcups2 samba samba-common # instalando o samba
+	sudo /etc/init.d/smbd restart # reinicializando o samba
+	sudo mkdir ~/compartilhado # criando a pasta para ser  compartilhada 
+    	sudo chmod -R 777 compartilhado #alterando as permissões
+	echo " 
 	[COMPATILHAMENTO]
-	path = /home/ubuntu/compSamba
+	path = /home/$USER/compSamba
 	public = yes
 	writeable = yes
 	browseable = yes" >> /etc/samba/smb.conf #inserindo o conteúdo no final do arquivo smb.conf.
 	smbd restart # restartando o serviço
-	ifconfig #veifiue o IP do servidor p/ poder acessar o servidor no cliente
-	adduser -a cmp #criando o usuário cmp no linux
-	smbpasswd -a cmp #criando o usuário cmp no samba
-	Menu
-}
+ 	ifconfig #veifiue o IP do servidor p/ poder acessar o servidor no cliente
+	sudo adduser -a cmp #criando o usuário cmp no linux
+	sudo -k smbpasswd -a cmp #criando o usuário cmp no samba
+	zenity --width=225 --height=100 --info \
+	--text="Configuração concluída, seu diretório compartilhado é foi criado."
+	
+elif [ $opcao -eq 6 ]; then	
+	
+	break
+fi
 
-Menu
+done
 
 
