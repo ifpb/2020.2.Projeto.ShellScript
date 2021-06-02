feb25709fc5da99e43f9eb97da151eddc9520841
Valesca Melo <valesca.melo@academico.ifpb.edu.br>
2021-06-01 23:24:16 -0300

    Atualização menu atributos adição e retirada.

--- Grupo_05/menu_atributos.sh
+++ Grupo_05/menu_atributos.sh
@@ -46,6 +46,39 @@ Menu_Atributos() {
 						adiciona-atributo-u $arquivo
 						verifica_atributo_u="6"
 					fi
+
+				done
+			#Condições para retirada de Atributos
+			if  [[ "$operador" == "1" ]]; then
+                                for (( i=0; i<${#atributos}; i++ )); do
+                                        #Condições de Retirada de Atributos
+                                        if [[ $atributos =~ 0 && "$verifica_atributo_A" == "0" ]]; then
+                                                retira-atributoA $arquivo
+                                                verifica_atributo_A="1"
+                                        fi
+                                        if [[ $atributos =~ 1 && "$verifica_atributo_a" == "1" ]]; then
+                                                retira-atributo-a $arquivo
+                                                verifica_atributo_a="2"
+                                        fi
+                                        if [[ $atributos =~ 2 && "$verifica_atributo_c" == "2" ]]; then
+                                                retira-atributo-c $arquivo
+                                                verifica_atributo_c="3"
+                                        fi
+                                        if [[ $atributos =~ 3 && "$verifica_atributo_i" == "3" ]]; then
+                                                retira-atributo-i $arquivo
+                                                verifica_atributo_i="4"
+                                        fi
+                                        if [[ $atributos =~ 4 && "$verifica_atributo_s" == "4" ]]; then
+                                                retira-atributo-s $arquivo
+                                                verifica_atributo_s="5"
+                                        fi
+                                        if [[ $atributos =~ 5 && "$verifica_atributo_u" == "5" ]]; then
+                                                retira-atributo-u $arquivo
+                                                verifica_atributo_u="6"
+                                        if [[ $atributos =~ 6 && "$verifica_atributo_todos" == "6" ]]; then
+						retira-todos-atributos $arquivo
+						verifica_atributo_todos="7"
+					fi
 				done
 			fi
 		fi
