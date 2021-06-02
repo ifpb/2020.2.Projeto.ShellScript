5f8511fdf24d9a13df49a10041ce8819bc687522
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2021-06-02 00:34:05 -0300

    Modificado config.sh

--- Grupo_01/src/config.sh
+++ Grupo_01/src/config.sh
@@ -12,18 +12,16 @@ if ! gtkdialog --help &>/dev/null && snap &>/dev/null; then
   s* | S* | '')
     sudo apt update
     echo "Instalando \e[31mGTKDialog\e[m e dependências..."
-    if ! dpkg -l build-essential &> /dev/null; then
-      echo -n "Instalando \e[31mbuild-essential\e[m..."
-      sudo -S apt install build-essential <<< "${PASSWORD}"
-      echo "OK"
-    fi
+    echo "Instalando \e[31mbuild-essential\e[m..."
+    sudo apt install build-essential
+    echo "OK"
     if ! dpkg -l libgtk2.0-dev &> /dev/null; then
-      echo -n "Instalando \e[31mlibgtk2.0-dev\e[m..."
-      sudo -S apt install libgtk2.0-dev <<< "${PASSWORD}"
+      echo "Instalando \e[31mlibgtk2.0-dev\e[m..."
+      sudo apt install libgtk2.0-dev
       echo "OK"
     fi
     if ! gtkdialog --help &> /dev/null; then
-      echo -n "Baixando \e[31mGTKDialog 0.8.3\e[m..."
+      echo "Baixando \e[31mGTKDialog 0.8.3\e[m..."
       mkdir ~/GTK && cd ~/GTK
       wget "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/gtkdialog/gtkdialog-0.8.3.tar.gz"
       tar -zxvf gtkdialog-0.8.3.tar.gz
@@ -32,15 +30,15 @@ if ! gtkdialog --help &>/dev/null && snap &>/dev/null; then
       echo -n "Configurando \e[31mGTKDialog 0.8.3\e[m..."
       ./configure
       make
-      sudo -S make install <<< "${PASSWORD}"
+      sudo make install
       echo "OK"
     fi
     if ! snap; then
-      echo -n "Instalando \e[31mSnap\e[m..."
+      echo "Instalando \e[31mSnap\e[m..."
       [[ ${OS} == "NAME=\"Linux Mint\"" ]] \
-      && sudo -S rm /var/lib/dpkg/lock-frontend <<< "${PASSWORD}" \
-      && sudo -S rm /var/cache/apt/archives/lock <<< "${PASSWORD}"
-      sudo -S apt install snapd <<< "${PASSWORD}"
+      && sudo rm /var/lib/dpkg/lock-frontend \
+      && sudo rm /var/cache/apt/archives/lock
+      sudo apt install snapd
       echo "OK"
     fi
     ;;
