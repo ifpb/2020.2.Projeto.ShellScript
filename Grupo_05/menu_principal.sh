02327df086c5072db5ed50b456c4ab18612b69fd
Valesca Melo <valesca.melo@academico.ifpb.edu.br>
2021-06-01 21:37:26 -0300

    Atualizando Menu Principal

--- Grupo_05/menu_principal.sh
+++ Grupo_05/menu_principal.sh
@@ -11,37 +11,21 @@ Menu_Principal() {
 		--text="Escolha uma das opções abaixo"                      \
 		--radiolist                                                 \
 		--hide-column=2                                             \
-       		--column="Marque" --column="" --column="Opções" false 0 "Modificar Permissão" false 1 "Modificar Atributo" false 2 "Help Stick Bits" false 3 "Help Chatrir")				
+       		--column="Marque" --column="" --column="Opções" false 0 "Modificar Permissão" false 1 "Modificar Atributo" false 2 "Help Stick Bits" false 3 "Help Chatrr")				
 
 case $escolha in
 	0)	
 		Menu_Permissoes
 		;;
 	1)
-		modo_arquivo="$(Menu_Tipo_Arquivo)"
-		
-		
-		if [ "$modo_arquivo" == "0" ]; then
-			arquivo="$(Menu_Selecionar_Arquivo)"
-			atributos="$(Menu_Selecionar_Atributos)"
-		fi
-
-		if [ "$modo_arquivo" == "1" ]; then
-			arquivos="$(Menu_Selecionar_Arquivos)"
-			atributos="$(Menu_Selecionar_Atributos)"
-
-		fi
-
-		if [ "$modo_arquivo" == "2" ]; then
-			diretorio="$(Menu_Selecionar_Diretorio)"
-			atributos="$(Menu_Selecionar_Atributos)"
-		fi
+		Menu_Atributos
 		;;
-	2)	
+
+	2)
 		Help_Sticky_Bit
 		;;
-	3)	
+	3)
 		echo "Você escolheu visualizr o help sobre Chattr"
-		
+
 esac
 }
